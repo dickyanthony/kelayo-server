04fa17e19e76a321e735249ede8dca7ade79d744
dickyanthony <dickyanthony0812@gmail.com>
2024-05-26 21:20:21 +0700

    DA-feat: add login register

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,67 @@
-import e from "express";
+import e from 'express';
 const router = e.Router();
 
-router.get("/", (req, res) => {
-  console.log("tes");
-  res.render("index", { text: "testing" });
+router.get('/', (req, res) => {
+  console.log('tes');
+  res.render('index', { text: 'testing' });
 });
 
-router.get("/new", (req, res) => {
-  res.send("User New Form");
+router.get('/new', (req, res) => {
+  res.send('User New Form');
+});
+
+//REGISTER
+router.post('/register', (req, res) => {
+  const { name, username, email, password, gender } = req.body;
+  const db = req.db;
+
+  if (!name || !username || !email || !password || !gender) {
+    return res.status(400).send('Isi Semua Bidang!');
+  }
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      return res.status(500).send('Oops, Terjadi permasalahan!');
+    }
+
+    const sql =
+      'INSERT INTO account (name, username, email, password, gender) VALUES (?, ?, ?, ?, ?)';
+    db.query(sql, [name, username, email, hash, gender], (err, result) => {
+      if (err) {
+        return res.status(500).send('Oops, Terjadi permasalahan!');
+      }
+      res.status(201).send('Daftar Berhasil');
+    });
+  });
+});
+
+//LOGIN
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const db = req.db;
+
+  if (!email || !password) {
+    return res.status(400).send('Isi Semua Bidang!');
+  }
+
+  const sql = 'SELECT id, email, name, gender FROM account WHERE email = ? AND password = ?';
+  db.query(sql, [email, password], (err, results) => {
+    if (err) {
+      return res.status(500).send('Oops, Terjadi permasalahan!');
+    }
+
+    if (results.length === 0) {
+      return res.status(401).send('Email atau password tidak sesuai');
+    }
+
+    const user = results[0];
+    res.status(200).json({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      gender: user.gender,
+    });
+  });
 });
 
 export default router;
--- server.js
+++ server.js
@@ -1,16 +1,47 @@
-import e from "express";
-import userRouter from "./routes/users.js";
+import e from 'express';
+import bodyParser from 'body-parser';
+import mysql from 'mysql';
+import bcrypt from 'bcryptjs';
+import cors from 'cors';
+import userRouter from './routes/users.js';
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'destination',
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySQL connected...');
+});
+
 const app = e();
 
 app.use(logger);
-app.set("view engine", "ejs");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  console.log("tes");
-  res.render("index", { text: "testing" });
+app.get('/', (req, res) => {
+  console.log('tes');
+  res.render('index', { text: 'testing' });
 });
 
-app.use("/users", userRouter);
+//USER ROUTER
+
+app.use(
+  '/users',
+  (req, res, next) => {
+    req.db = db;
+    next();
+  },
+  userRouter
+);
 
 function logger(req, res, next) {
   console.log(req.originalUrl);
