7b6e99825b7c63313cf365c8fa469168e06bc8a6
dickyanthony <dickyanthony0812@gmail.com>
2024-05-30 20:33:04 +0700

    DA-feat: add delete user

--- routes/auth.js
+++ routes/auth.js
@@ -103,4 +103,24 @@ router.post('/login', (req, res) => {
   });
 });
 
+router.delete('/user/:id', (req, res), () => {
+  const db = req.db;
+  const { id } = req.params;
+  const sql = 'DELETE FROM account WHERE id = ?';
+
+  db.query(sql, [id], (err, results) => {
+    if (err) {
+      res.status(500).send('Oops, Terjadi permasalahan!');
+      return;
+    }
+
+    if (results.affectedRows === 0) {
+      res.status(404).send('Data tidak ditemukan!');
+      return;
+    }
+
+    res.json({ message: 'Data berhasil dihapus!' });
+  });
+});
+
 export default router;
